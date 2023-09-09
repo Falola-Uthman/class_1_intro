6d1da418051f21d0e25a4b34526fabf0ed0f3c14
Falola-Uthman <falolauthman@gmail.com>
2023-09-09 16:54:30 -0400

    Class 2 First commit

---
+++ bin/class_1_intro.dart
@@ -0,0 +1,5 @@
+import 'package:class_1_intro/class_1_intro.dart' as class_1_intro;
+
+void main(List<String> arguments) {
+ print("heyy");
+}
---
+++ test/class_1_intro_test.dart
@@ -0,0 +1,8 @@
+import 'package:class_1_intro/class_1_intro.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
