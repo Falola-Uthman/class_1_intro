8cdd86a77a0688106b8d30809c4fa21c1738c481
enoch-aik <enoch_aik@icloud.com>
2023-09-11 23:07:19 +0200

    - added assignments

--- bin/class_2.dart
+++ bin/class_2.dart
@@ -1,5 +1,5 @@
 //we would be learning functions and conditions in programming lang
-
+//loops and classes
 
 //int, string, list, bool, set, map, double
 
@@ -9,7 +9,14 @@ int multiplyTwo ({required int firstNumber, required int secondNumber}){
  int answer = firstNumber * secondNumber;
  return answer;
 }
-int divideTwo ({required int numberOne, required int numberTwo}){
+int divideTwoNumbers ({required int numberOne, required int numberTwo}){
  int result = numberOne ~/ numberTwo;
  return result;
 }
+
+
+///assignment
+// create a function to calculate quadratic equations, tip: it should accept 3 numbers for a, b and c respectively
+// the end result should be two possible values
+
+b
--- bin/main.dart
+++ bin/main.dart
@@ -5,6 +5,29 @@ void main(List<String> arguments) {
   //class1Intro(arguments);
 
 print(multiplyTwo(firstNumber:2, secondNumber: 3));
-print(divideTwo(numberOne:4,numberTwo: 3));
+
+int divisionResult = divideTwoNumbers(numberOne:4,numberTwo: 3);
+
+if (divisionResult <= 5 || (divisionResult != 1)){
+  print('division result is less than 5');
+} else if (divisionResult == 1 ){
+  print('division result is equal to 1');
+}
+else {
+  print('division result is greater than 5');
+
+}
+
+
+// OR ||
+// AND &&
+// greater than >
+// less than <
+// greater than or equal to >=
+// less than or equal to <=
+// equal to ==
+// not equal to !=
+// and
+
 
 }
